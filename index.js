99d1cb1184a85fd1c56ee89cb92f16dada0625df
Hugo-Alonso <hugoalonsotoledo12@gmail.com>
2025-01-01 19:55:19 +0100

    Improve server with historical data of messages

--- index.js
+++ index.js
@@ -7,25 +7,44 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+const PORT = 3000;
+const messageHistory = [];
+const users = new Map();
+
 app.get('/', (req, res) => {
-    // res.send('<h1>Hello world</h1>');
     res.sendFile(join(__dirname, 'index.html'));
-})
+  });
+
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
+    console.log('A user connected:', socket.id);
 
-        io.emit('chat message', msg);
-      });
-      
+    // Asignar un nombre de usuario único
+    const username = `User${Math.floor(Math.random() * 1000)}`;
+    users.set(socket.id, username);
+    socket.emit('system message', `Welcome, ${username}!`);
+
+    // Enviar historial de mensajes al nuevo usuario
+    socket.emit('message history', messageHistory);
+
+    // Escuchar mensajes
+    socket.on('chat message', (msg) => {
+        const userMessage = `${users.get(socket.id)}: ${msg}`;
+        messageHistory.push(userMessage);
+
+        // Limitar historial a los últimos 50 mensajes
+        if (messageHistory.length > 50) messageHistory.shift();
+
+        io.emit('chat message', userMessage);
+    });
+
+    // Desconexión
     socket.on('disconnect', () => {
-      console.log('user disconnected');
+        console.log('A user disconnected:', socket.id);
+        users.delete(socket.id);
     });
 });
 
-server.listen(3000, () => {
-    console.log('server running at http://localhost:3000');
-})
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
